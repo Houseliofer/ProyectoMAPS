a3477d3c7e63ea76b9e08a6b932aa7485f67bab4
Houseliofer <20030392@itcelaya.edu.mx>
2023-06-04 23:29:40 -0600

    Primer COmmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+//Maps
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [map, setMap] = useState(null);
+  const [searchInput, setSearchInput] = useState('');
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    // Cargar el mapa de Google Maps
+    const loadMap = () => {
+      const googleMapsScript = document.createElement('script');
+      googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAOEH-FZHo_RnidhHMigrUFGvSRseFuTJo&libraries=places`;
+      googleMapsScript.onload = initMap;
+      document.head.appendChild(googleMapsScript);
+    };
+
+    loadMap();
+  }, []);
+
+  const initMap = () => {
+    // Crear el mapa y establecer los eventos
+    const mapOptions = {
+      center: { lat: 0, lng: 0 },
+      zoom: 2,
+    };
+    const mapElement = document.getElementById('map');
+    const newMap = new window.google.maps.Map(mapElement, mapOptions);
+    setMap(newMap);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Buscador de Lugares</h1>
+      <div id="map" style={{ height: '400px', marginTop: '20px' }}></div>
+      {places.length > 0 && (
+        <ul>
+          {places.map((place) => (
+            <li key={place.place_id}>{place.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
